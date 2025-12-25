216ea5eff0e506e6448dec9f26df63d93e7405b6
nopparat <nopparat.cysit@gmail.com>
2025-12-25 15:17:14 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <section>
+      <h1>My First React App on Vite</h1>
+    </section>
+  )
+}
+
+export default App
